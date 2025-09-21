d5d2883348f3ccf05cd88f7bd65f3ef23b72eecd
Notorious <100317984+Ian9Franco@users.noreply.github.com>
2025-09-21 03:11:44 -0300

    2.1 commit

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>Produ Estudio</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  )
+}
